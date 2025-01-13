7d17738b7348235ce4c5274399e3bb4ef51bacb2
jibin jose <jibinjose884@gmail.com>
2025-01-13 09:08:33 +0530

    Add files via upload

---
+++ ASCII_Value.py
@@ -0,0 +1,19 @@
+# Python Program to Find ASCII Value of Character.
+# (Use of ORD() Function)
+Sag = "J"
+print("The ASCII Value of", Sag, "is", ord(Sag))
+
+sag = "I"
+print("The ASCII Value of", sag, "is", ord(sag))
+
+Moh = "B"
+print("The ASCII Value of", Moh, "is", ord(Moh))
+
+moh = "I"
+print("The ASCII Value of", moh, "is", ord(moh))
+
+Gos = "N"
+print("The ASCII Value of", Gos, "is", ord(Gos))
+
+gos = "#"
+print("The ASCII Value of", gos, "is", ord(gos))
