a813215bf598111e66cf18ac821b367b8e6068db
jibin jose <jibinjose884@gmail.com>
2025-01-14 22:40:14 +0530

    Add files via upload

---
+++ Add_Two_Numbers.py
@@ -0,0 +1,14 @@
+# Write a Program Add Two Numbers in Python.
+# 1st Method (With Pre-defined Variables)
+# num1 = 45
+# num2 = 27
+# sum = num1 + num2
+# print("Sum is: ", sum)
+
+
+
+# 2nd Method (With User Inputs)
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number: "))
+sum = num1 + num2
+print("sum is: ", sum)
