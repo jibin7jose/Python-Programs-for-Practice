980d234e7c9a136518c816005dd725e2fe614f3f
jibin jose <jibinjose884@gmail.com>
2025-01-19 22:50:36 +0530

    Add files via upload

---
+++ Count_the_Occurrence.py
@@ -0,0 +1,21 @@
+# # Python Program to Count the Occurrence of an Item in a List.
+# numbers = [10,20,30,40,50,60,70,80,90,30,40]
+# count_occurrence = numbers.count(40)
+# print(count_occurrence)
+
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 30, 40]
+
+count_30 = numbers.count(30)
+count_40 = numbers.count(40)
+count_50 = numbers.count(50)
+
+print("Occurrence of 30:", count_30)
+print("Occurrence of 40:", count_40)
+print("Occurrence of 50:", count_50)
+
+
+# output
+# Occurrence of 30: 2
+# Occurrence of 40: 2
+# Occurrence of 50: 1
